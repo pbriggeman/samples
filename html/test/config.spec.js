c18f5a93db65d94381fc2d083a24df12cae1bc2a
Peter Biggeman <pbriggeman@gmail.com>
2017-02-03 16:09:15 -0500

    Added NodeJS app to framework

--- html/test/config.spec.js
+++
@@ -1,7 +0,0 @@
-describe("CONFIG SETTINGS", function() {
-    var config = require('../config')();
-    it("Should load app name", function(next) {
-        expect(config.app_name).toBe('Portfolio');
-        next();
-    });
-});
